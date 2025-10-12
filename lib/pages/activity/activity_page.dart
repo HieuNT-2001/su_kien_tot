437d4ec9ae564bd27b3012a557050847905b77ec
HieuNT-2001 <hieuth0109@gmail.com>
2025-10-12 21:39:27 +0700

    Revert "Update activity_page.dart"
    This reverts commit 9d34e62f974edfed33eb83dcb5f2da41d2a435b8.

--- lib/pages/activity/activity_page.dart
+++ lib/pages/activity/activity_page.dart
@@ -13,7 +13,7 @@ class ActivityPage extends StatefulWidget {
 
 class _ActivityPageState extends State<ActivityPage> {
   bool _isLoading = true;
-  List<String> _activities = [];
+  List<String> activities = [];
 
   @override
   void initState() {
@@ -24,21 +24,24 @@ class _ActivityPageState extends State<ActivityPage> {
   Future<void> _refreshData() async {
     setState(() => _isLoading = true);
     await Future.delayed(const Duration(seconds: 2)); // giả lập call API
-    _activities = ['hoạt động 1'];
-    // bạn có thể thử: ["Hoạt động 1", "Hoạt động 2"]
-    setState(() => _isLoading = false);
+    setState(() {
+      _isLoading = false;
+      activities = [
+        'hoạt động 1',
+      ]; // bạn có thể thử: ["Hoạt động 1", "Hoạt động 2"]
+    });
   }
 
   Future<void> _emptyData() async {
     setState(() => _isLoading = true);
     await Future.delayed(const Duration(seconds: 2)); // giả lập call API
-    _activities = [];
-
-    setState(() => _isLoading = false);
+    setState(() {
+      _isLoading = false;
+      activities = [];
+    });
   }
 
-  Widget _buildDataSection() {
-    int length = _isLoading ? 4 : _activities.length;
+  Widget _buildDataSection(int length) {
     if (length == 0) return const EmptyItem(title: 'Chưa có hoạt động');
 
     return Column(
@@ -80,7 +83,7 @@ class _ActivityPageState extends State<ActivityPage> {
                   ],
                 ),
                 const SizedBox(height: 16),
-                _buildDataSection(),
+                _buildDataSection(_isLoading ? 4 : activities.length),
               ],
             ),
           ),
